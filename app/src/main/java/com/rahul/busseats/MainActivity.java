60a84280424d7a34369ea2b1f7bd8db6124bb8ae
rahul6975 <rahulya7569@gmail.com>
2021-09-13 10:35:02 +0530

    grid layout added

--- app/src/main/java/com/rahul/busseats/MainActivity.java
+++ app/src/main/java/com/rahul/busseats/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rahul.busseats;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -35,28 +36,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView1 = findViewById(R.id.recyclerview1);
-        recyclerView2 = findViewById(R.id.recyclerview2);
-        recyclerView3 = findViewById(R.id.recyclerview3);
-        recyclerView4 = findViewById(R.id.recyclerview4);
 
         addToList();
         ViewAdapter1 viewAdapter1 = new ViewAdapter1(list4);
-        ViewAdapter2 viewAdapter2 = new ViewAdapter2(list3);
-        ViewAdapter3 viewAdapter3 = new ViewAdapter3(list1);
-        ViewAdapter4 viewAdapter4 = new ViewAdapter4(list0);
 
-        LinearLayoutManager linearLayoutManager1 = new LinearLayoutManager(this);
-        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(this);
-        LinearLayoutManager linearLayoutManager3 = new LinearLayoutManager(this);
-        LinearLayoutManager linearLayoutManager4 = new LinearLayoutManager(this);
-        recyclerView1.setLayoutManager(linearLayoutManager1);
-        recyclerView2.setLayoutManager(linearLayoutManager2);
-        recyclerView3.setLayoutManager(linearLayoutManager3);
-        recyclerView4.setLayoutManager(linearLayoutManager4);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 4);
+        recyclerView1.setLayoutManager(gridLayoutManager);
         recyclerView1.setAdapter(viewAdapter1);
-        recyclerView2.setAdapter(viewAdapter2);
-        recyclerView3.setAdapter(viewAdapter3);
-        recyclerView4.setAdapter(viewAdapter4);
     }
 
     private void addToList() {
@@ -71,16 +57,15 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject b = itemObj.getJSONObject("1");
                 JSONObject c = itemObj.getJSONObject("3");
                 JSONObject d = itemObj.getJSONObject("4");
-                String e = a.getString("n");
-                String f = b.getString("n");
-                String g = c.getString("n");
-                String h = d.getString("n");
+                String h = a.getString("n");
+                String g = b.getString("n");
+                String k = c.getString("n");
+                String j = d.getString("n");
 
                 list4.add(h);
-                list3.add(g);
-                list1.add(f);
-                list0.add(e);
-
+                list4.add(g);
+                list4.add(k);
+                list4.add(j);
             }
 
         } catch (JSONException | IOException e) {
